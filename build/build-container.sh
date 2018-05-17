ab91a07dcbd96cfa2cd869145af516d2b6cec501
Rushikesh Badami <rushikesh.badami@sonos.com>
2018-05-17 16:35:35 -0700

    Added test boilerplate ping script

--- build/build-container.sh
+++ build/build-container.sh
@@ -1,1 +1,12 @@
-echo "Able to ping container script"
+python3 -m venv job-deployement
+rm -rf job-deployement
+rm -rf lib
+python3 -m venv job-deployement
+source job-deployement/bin/activate
+#requirement="requirement.txt"
+while IFS= read -r dependency
+do
+  pip3 install requests $dependency
+done < "$requirement"
+mkdir lib
+mv job-deployement/lib/python3.6/site-packages/* lib/
