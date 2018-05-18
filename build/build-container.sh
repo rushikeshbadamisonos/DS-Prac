5fb0dc814a454dd0c7ef86cd12be60a2ff67224e
Rushikesh Badami <rushikesh.badami@sonos.com>
2018-05-18 16:26:20 -0700

    Added something

--- build/build-container.sh
+++ build/build-container.sh
@@ -5,9 +5,7 @@ rm -rf job-deployement
 python3 -m venv job-deployement
 python3 -m venv job-deployement
 source job-deployement/bin/activate
-rm -rf ../container ../build/job-deployement ../build/libs/
-rm -rf ../deploymentContainer.zip
-requirement="../src/jobs/"$1"/Config/requirements.txt"
+requirement="src/jobs/"$1"/Config/requirements.txt"
 while IFS= read -r dependency
 do
   pip3 install requests $dependency
