57e4f0f4190ad7d19b5cf0b1937a03d0059f4e07
Rushikesh Badami <rushikesh.badami@sonos.com>
2018-05-18 15:48:19 -0700

    Added something

--- build/build-container.sh
+++ build/build-container.sh
@@ -1,6 +1,7 @@
 python3 -m venv job-deployement
 python3 -m venv job-deployement
 source job-deployement/bin/activate
+rm -rf ../container ../build/job-deployement ../build/libs/
 rm -rf ../deploymentContainer.zip
 requirement="../src/jobs/"$1"/Config/requirements.txt"
 while IFS= read -r dependency
