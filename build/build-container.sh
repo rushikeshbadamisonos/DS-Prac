eb64a216ab33a0ae759379e8431c264adc901749
Rushikesh Badami <rushikesh.badami@sonos.com>
2018-05-21 11:42:16 -0700

    Added something

--- build/build-container.sh
+++ build/build-container.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-source job-deployement/bin/activate
-
-requirement="src/jobs/"wordcount"/Config/requirements.txt"
-while IFS= read -r dependency
-do
-  pip install --ignore-installed --user $dependency
-  done < "$requirement"
 cd job-deployement/lib/python3.6/site-packages/
 ls -l
