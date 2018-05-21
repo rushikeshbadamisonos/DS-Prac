d67f0b5f2635db5e632301031dcba0416d5a1f46
Rushikesh Badami <rushikesh.badami@sonos.com>
2018-05-21 11:32:41 -0700

    Added something

--- build/build-container.sh
+++ build/build-container.sh
@@ -6,13 +6,5 @@ while IFS= read -r dependency
 do
   pip install --ignore-installed --user $dependency
   done < "$requirement"
-  cd job-deployement/lib/python3.6/
-ls -l
-mv job-deployement/lib/python3.6/site-packages/* libs/
-cd libs && zip -x main.py -r "../container/libs.zip"  .
-cd ../src/  && zip -x main.py --exclude = "*libs*" -r "../container/jobs.zip"  .
-cp main.py ../container/
-cd ../container && zip -r "../deploymentContainer_"$1".zip" .
-cd ..
-cd libs
+cd job-deployement/lib/
 ls -l
