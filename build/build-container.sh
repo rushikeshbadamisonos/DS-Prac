e12c612b62229dec55dcd3d13067fdb7048a1e84
Rushikesh Badami <rushikesh.badami@sonos.com>
2018-05-21 10:15:08 -0700

    Added something

--- build/build-container.sh
+++ build/build-container.sh
@@ -1,9 +1,3 @@
 mv job-deployement/lib/python3.6/site-packages/* libs/
-pwd
-ls -l
 cd libs && zip -x main.py -r "../container/libs.zip"  .
-cd ../../src/  && zip -x main.py --exclude = "*libs*" -r "../container/jobs.zip"  .
-cp main.py ../container/
-cd ../container && zip -r "../deploymentContainer_"$1".zip" .
-rm -rf ../container ../build/job-deployement ../build/libs/
 ls -l
