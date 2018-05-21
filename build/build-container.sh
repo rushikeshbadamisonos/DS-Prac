8deb99aa26084d24233d1d7407492d4172af4b64
Rushikesh Badami <rushikesh.badami@sonos.com>
2018-05-21 13:58:05 -0700

    Test

--- build/build-container.sh
+++ build/build-container.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-echo $SHELL
-cd job-deployement/lib/python3.6/site-packages/
-ls -l
+mv job-deployement/lib/python3.6/site-packages/* libs/
+
+cd libs && zip -x main.py -r "../container/libs.zip"  .
+cd ../src/  && zip -x main.py --exclude = "*libs*" -r "../container/jobs.zip"  .
+cp main.py ../container/
+cd ../container && zip -r "../deploymentContainer_"$1".zip" .
