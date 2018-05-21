b39e1c2f0382275dd1024082ae51b813c6df5dfd
Rushikesh Badami <rushikesh.badami@sonos.com>
2018-05-21 09:58:30 -0700

    Added something

--- build/build-container.sh
+++ build/build-container.sh
@@ -8,6 +8,8 @@ mkdir libs
 mkdir container
 mv job-deployement/lib/python3.6/site-packages/* libs/
 ls -l
+chmod 777 container
+chmod 777 libs
 cd libs && zip -x main.py -r "container/libs.zip"  .
 cd ../../src/  && zip -x main.py --exclude = "*libs*" -r "../container/jobs.zip"  .
 cp main.py ../container/
