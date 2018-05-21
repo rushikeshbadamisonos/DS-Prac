c3ed65a4e424e7e66473f0e119de91c5304852e3
Rushikesh Badami <rushikesh.badami@sonos.com>
2018-05-21 10:02:02 -0700

    Added something

--- build/build-container.sh
+++ build/build-container.sh
@@ -1,3 +1,4 @@
+pwd
 source job-deployement/bin/activate
 requirement="src/jobs/"$1"/Config/requirements.txt"
 while IFS= read -r dependency
@@ -7,8 +8,7 @@ done < "$requirement"
 mkdir libs
 mkdir container
 mv job-deployement/lib/python3.6/site-packages/* libs/
-cd libs
-ls -l
+
 cd libs && zip -x main.py -r "container/libs.zip"  .
 cd ../../src/  && zip -x main.py --exclude = "*libs*" -r "../container/jobs.zip"  .
 cp main.py ../container/
