a6071b3cddbbac67045870141fdeb97212859e46
Rushikesh Badami <rushikesh.badami@sonos.com>
2018-05-21 14:13:04 -0700

    Test

--- build/build-container.sh
+++ build/build-container.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mv job-deployement/lib/python3.6/site-packages/* libs/
-
+cp /home/jenkins/.local/lib/python3.6/site-packages/*  libs/
 cd libs && zip -x main.py -r "../container/libs.zip"  .
 cd ../src/  && zip -x main.py --exclude = "*libs*" -r "../container/jobs.zip"  .
 cp main.py ../container/
