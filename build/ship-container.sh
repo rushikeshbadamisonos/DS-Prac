032c70f31f362e0786c80f95e9a85bcc1f43915d
Rushikesh Badami <rushikesh.badami@sonos.com>
2018-05-22 11:17:17 -0700

    Added test

--- build/ship-container.sh
+++ build/ship-container.sh
@@ -1,3 +1,3 @@
-ssh -i -o StrictHostKeyChecking=no $1 $2 'ls -l'
+ssh -i -o StrictHostKeyChecking=no $2 'ls -l'
 #ssh -i $3   $2 "mkdir ~/deploymentContainer_$1"
 #scp -i $3   ../"deploymentContainer_"$1".zip" $2:~/"deploymentContainer_$1"
