67cfb225bc5748f7a514e3937d0dfd8f3f8f7337
Rushikesh Badami <rushikesh.badami@sonos.com>
2018-05-21 17:11:14 -0700

    Added test

--- build/ship-container.sh
+++ build/ship-container.sh
@@ -1,2 +1,3 @@
-ssh -i $3   $2 "mkdir ~/deploymentContainer_$1"
-scp -i $3   ../"deploymentContainer_"$1".zip" $2:~/"deploymentContainer_$1"
+ssh -i $1 hadoop@$2 'ls -l'
+#ssh -i $3   $2 "mkdir ~/deploymentContainer_$1"
+#scp -i $3   ../"deploymentContainer_"$1".zip" $2:~/"deploymentContainer_$1"
